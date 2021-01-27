5bdfc83be35c5c51139e097da63241da42ae6bd2
Rajesh Sah <huntsmanrj234@gmail.com>
2021-01-27 11:11:02 +0545

    Initial Commit

---
+++ Patterns/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Patterns
+//
+//  Created by Rajesh Sah on 27/01/21.
+//  Copyright © 2021 Rajesh Sah. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
